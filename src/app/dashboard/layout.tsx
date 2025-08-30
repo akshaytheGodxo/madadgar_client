375aa3b4ec1671c653a796cef97fc95e55a6c0ce
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-08-30 14:57:52 +0530

    done

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,12 +3,14 @@ import { AppSidebar } from "@/components/ui/AppSidebar"
 import { Suspense } from "react"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="w-full">
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+    <Suspense>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="w-full">
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </Suspense>
   )
 }
