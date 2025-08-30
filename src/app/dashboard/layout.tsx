d9b45ec4d1fbae7978cb55ea7979bb352c8404fe
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-08-30 14:51:36 +0530

    done

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/Sidebar"
 import { AppSidebar } from "@/components/ui/AppSidebar"
-
+import { Suspense } from "react"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,26 +7,26 @@ import ChooseOrg from "./akshansh_to_do/choose_org";
 import Pending from "./akshansh_to_do/Pending";
 import Games from "./akshansh_to_do/games";
 
-import { Suspense } from "react";
+
 
 export default function Dashboard() {
     const searchParams = useSearchParams();
     const tab = searchParams.get("tab");
     return (
-        <Suspense fallback={<div className="p-6">Loading...</div>}>
-            <div className=" w-full h-screen p-6 bg-gray-50">
-                {tab === "home" && (<Home />)}
-                {tab === "drills" && (<Drills />)}
-                {tab === "games" && (<Games />)}
-                {tab === "connect"}
-                {tab === "settings"}
-                {tab === "choose_org" && (<ChooseOrg />)}
-                {tab === "pending" && (<Pending />)}
-                <iframe
-                    src="/chatBot/index.html"
-                    className="fixed bottom-4 right-4 w-[400px] h-[500px] "
-                ></iframe>
-            </div>
-        </Suspense>
+        <div className=" w-full h-screen p-6 bg-gray-50">
+            {tab === "home" && (<Home />)}
+            {tab === "drills" && (<Drills />)}
+            {tab === "games" && (<Games />)}
+            {tab === "connect"}
+            {tab === "settings"}
+            {tab === "choose_org" && (<ChooseOrg />)}
+            {tab === "pending" && (<Pending />)}
+
+
+            <iframe
+                src="/chatBot/index.html"
+                className="fixed bottom-4 right-4 w-[400px] h-[500px] "
+            ></iframe>
+        </div>
     );
 }
