afb10a65701562ebe032414f5aef703cf44b27dd
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-08-31 10:23:45 +0530

    d

--- src/app/dashboard/akshansh_to_do/games.tsx
+++ src/app/dashboard/akshansh_to_do/games.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link"
+
 export default function Games() {
     return (
-        <div className="p-8  min-h-screen text-white">
-            <button className="bg-green-500 w-full h-14">
-                <Link href="http://2d-game-eight.vercel.app " target="_blank">Start Playing</Link>
-            </button>
+        <div className="p-8 min-h-screen text-white flex flex-col gap-6 items-stretch">
+            <Link href="http://2d-game-eight.vercel.app" target="_blank" className="w-full">
+                <button className="bg-green-500 w-full py-6 rounded text-white text-lg shadow hover:bg-green-600 transition">
+                    3D Disaster Response Training
+                </button>
+            </Link>
+            <Link href="https://quizgame-weld.vercel.app/" target="_blank" className="w-full">
+                <button className="bg-green-500 w-full py-6 rounded text-white text-lg shadow hover:bg-green-600 transition">
+                    Quiz Game
+                </button>
+            </Link>
         </div>
     )
 }
