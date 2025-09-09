fcf95da0adfc04b878797b92a75dee57d01994f6
akshaytheGodxo <akshaysinghc230@gmail.com>
2025-09-09 20:10:50 +0530

    reframed and rewired

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 import Navbar from "@/components/ui/Nav";
-import Hero from "@/components/new-landing/Hero1";
-import Problems from "@/components/new-landing/Problems";
-import Vision from "@/components/new-landing/Vision";
-import Features from "@/components/new-landing/Features";
-import Workflow from "@/components/new-landing/Workflow";
-import Architecture from "@/components/new-landing/Architecture";
-import Validation from "@/components/new-landing/Validation";
-import CTA from "@/components/new-landing/CTA";
+
+import Hero1 from "@/components/landing/Hero1";
 export default function Home() {
   return (
     <>
       <Navbar/>
-      <Hero />
-      <Problems />
-      <Vision />
-      <Features />
-      <Workflow />
-      <Architecture />
-      <Validation />
-      <CTA />
+      <Hero1 />
     </>
   );
 }
